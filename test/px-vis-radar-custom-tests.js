b3ecd314ef94e102ed6b1e9a23bf040c4a69b0cd
Vermel, Runn (GE Global Research) <runn.vermel@ge.com>
2016-10-10 10:02:28 -0700

    commented out default test

--- test/px-vis-radar-custom-tests.js
+++ test/px-vis-radar-custom-tests.js
@@ -4,24 +4,24 @@ function runCustomTests() {
 
   // This is the placeholder suite to place custom tests in
   // Use testCase(options) for a more convenient setup of the test cases
-  suite('Custom Automation Tests for px-vis-radar', function() {
-    test('Check initial value of counter', function(done){
-      var counterEl = Polymer.dom(document).querySelector('px-vis-radar'),
-          counterValueEl = Polymer.dom(counterEl.root).querySelector('span');
-      assert.equal(counterValueEl.textContent, '0');
-      done();
-    });
-
-    test('Clicking px-vis-radar increments the counter', function(done){
-      var counterEl = Polymer.dom(document).querySelector('px-vis-radar'),
-          counterValueEl = Polymer.dom(counterEl.root).querySelector('span');
-      assert.equal(counterValueEl.textContent, '0');
-
-      counterEl.click();
-      flush(function(){
-        assert.equal(counterValueEl.textContent, '1');
-      });
-      done();
-    });
-  });
-};
+  // // suite('Custom Automation Tests for px-vis-radar', function() {
+  // //   test('Check initial value of counter', function(done){
+  // //     var counterEl = Polymer.dom(document).querySelector('px-vis-radar'),
+  // //         counterValueEl = Polymer.dom(counterEl.root).querySelector('span');
+  // //     assert.equal(counterValueEl.textContent, '0');
+  // //     done();
+  // //   });
+  // //
+  // //   test('Clicking px-vis-radar increments the counter', function(done){
+  // //     var counterEl = Polymer.dom(document).querySelector('px-vis-radar'),
+  // //         counterValueEl = Polymer.dom(counterEl.root).querySelector('span');
+  // //     assert.equal(counterValueEl.textContent, '0');
+  // //
+  // //     counterEl.click();
+  // //     flush(function(){
+  // //       assert.equal(counterValueEl.textContent, '1');
+  // //     });
+  // //     done();
+  // //   });
+  // });
+}
